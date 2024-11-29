381cf9a1fbfea14322bc0e281e279c20a9533bc9
DeyvidA <darauz@nicarao.agency>
2024-11-28 23:53:41 -0600

    FIX DATABASE CONNECTION BY ENV

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,13 +1,21 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import config from 'src/config';
 
 @Global()
 @Module({
   imports: [
-    MongooseModule.forRoot(`mongodb://localhost:27017`, {
-      pass: 'example',
-      user: 'root',
-      dbName: 'nest',
+    MongooseModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { database } = configService;
+
+        return {
+          uri: `mongodb://${database.userName}:${database.password}@${database.host}:${database.port}`,
+          dbName: database.dbName,
+        };
+      },
     }),
   ],
   exports: [MongooseModule],
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -20,5 +20,6 @@ export const databaseProviders = [
         throw error;
       }
     },
+    inject: [config.KEY],
   },
 ];
